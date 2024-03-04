5b9b9ce97b1843722120a02ce967b288913b054a
olivia1117 <oadamic@gmail.com>
2024-03-04 11:31:44 -0600

    test

--- index.js
+++ index.js
@@ -17,12 +17,50 @@ app.get('/about', (request, response) => {
 	response.send('This is a website that utilizes server-side node.js to implement an insurance risk calculator.')
 })
 
+let ageResult = {value : 0};
+//let ageResult; 
+
 app.get('/age', (request, response) => {
-	console.log('Calling "/age"')
-  
-	response.type('text/plain')
-	response.send('age')
+	console.log('Calling "/age"');
+
+	response.type('text/plain');
+	response.send(ageResult.value.toString());
 })
+
+
+app.post('/age', (req, res) => {
+  // Assuming the client sends age as a JSON object like { "age": 25 }
+  const age = req.body.age;
+  
+  function calculateInsuranceRisk(age) {
+    if (age == 0) {
+      ageResult.value = 0; 
+      // return {value : 0};
+    }
+    else if (age == 10){
+      ageResult.value = 10;
+      
+      // return {value : 10};
+    }
+    // else if (age == 20){
+    //   return ageResult.value = 20; 
+    // }
+    // else{
+    //   return ageResult.value = 30; 
+    // }
+  }
+  //const insuranceRiskAge = calculateInsuranceRisk(age);
+  // let ageResult = {value : 0}
+  calculateInsuranceRisk(age);
+  
+
+  // Send the calculated result back to the client
+  // res.json(ageResult);
+  res.send(ageResult.value.toString());
+});
+
+
+
 
 app.get('/api/ping', (request, response) => {
 	console.log('Calling "/api/ping"')
